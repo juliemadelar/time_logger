8f745579a45401eb054fd376d05b25a2716842fa
Jereme <jeremejazz15@gmail.com>
2025-03-26 15:53:15 +0800

    chore: cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:time_logger/timesheet_page.dart';
 import 'home_page.dart';
 
 void main() {
